a8e148a0a9e695494fefd00d896a4fd27bf81510
morintu <morintu@gmail.com>
2013-05-29 00:07:25 +0900

    The forgotten comment.

--- src/jp/morintu/game/HakoiriDoroidKun/Function.java
+++ src/jp/morintu/game/HakoiriDoroidKun/Function.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2013 Morintu. All Rights Reserved.
+ * Do not alter or remove
+ * Copyright notices or this file header.
+ * [著作権表示の変更・削除、または本ファイルのヘッダの削除を禁ずる。]
+ */
 package jp.morintu.game.HakoiriDoroidKun;
 
 import jp.morintu.game.HakoiriDoroidKun.parts.Vector2;
